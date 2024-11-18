a61911a656ccc68607b86b0fa7cd7b3f3678e57a
Thomas Linssen <t.linssen@live.nl>
2024-11-18 18:31:45 +0100

    Added "still empty here" message

--- src/app/mywatchlist/page.tsx
+++ src/app/mywatchlist/page.tsx
@@ -44,33 +44,52 @@ export default async function MyWatchList() {
                 <div className=" w-full h-full relative group">
                     <img className="object-cover w-[185px] aspect-[2/3]" src={item.movie.posterPath} 
                     alt={`${item.movie.title}_thumbnail.png`}/>
-                    <div className="group-hover:opacity-75 transition-opacity opacity-0 absolute top-0 left-0 bg-black h-full w-full flex justify-center items-center">
+                    <div className="group-hover:opacity-75 transition-opacity opacity-0 absolute
+                     top-0 left-0 bg-black h-full w-full flex justify-center items-center">
                         <p>[description]</p>
                     </div>
                 </div>
                 <h3 className="text-l mt-2 text-center">{item.movie.title}</h3>
 
                 {/* Show your rating as well if you've watched it */}
-                {item.watched ? <h5 className="text-sm opacity-50">You rated it: 3.4 Stars</h5> : <div></div>}
+                {item.watched ? <h5 
+                className="text-sm opacity-50">You rated it: 3.4 Stars</h5> : <div></div>}
                 
             </div>);
     }
 
-    // Finally, display both the watched and unwatched movies in separate sections
+    // If page content is empty, show it on the screen. Otherwise, show the lists of movies.
+    let pageContent;
+    if(watchlist.length == 0)
+        pageContent = 
+        <div>
+            <h2 className="text-3xl">Still empty here...</h2>
+            <p>Add some movies to make them show up here!</p>
+        </div>
+    else
+    // Display both the watched and unwatched movies in separate sections
+        pageContent = (
+            <div>
+                <div className="p-4">
+                    <h1 className="text-2xl">Watched</h1>
+                    <div className="flex gap-5 mt-2 py-4 items-start flex-wrap">
+                        {mapMoviesToList(watchedMovies)}
+                    </div>
+                </div>
+                <div className="p-4">
+                    <h1 className="text-2xl">Plan to watch</h1>
+                    <div className="flex gap-5 mt-2 py-4 items-start flex-wrap">
+                        {mapMoviesToList(planToWatchMovies)}
+                    </div>
+                </div>
+            </div>
+        )
+
+    // Finally, return the content
     return (
-        <div className="flex flex-col items-left py-10 px-10 bg-background_secondary font-[family-name:var(--font-alatsi)]">
-            <div className="p-4">
-                <h1 className="text-2xl">Watched</h1>
-                <div className="flex gap-5 mt-2 py-4 items-start flex-wrap">
-                    {mapMoviesToList(watchedMovies)}
-                </div>
-            </div>
-            <div className="p-4">
-                <h1 className="text-2xl">Plan to watch</h1>
-                <div className="flex gap-5 mt-2 py-4 items-start flex-wrap">
-                    {mapMoviesToList(planToWatchMovies)}
-                </div>
-            </div>
+        <div className="flex flex-col items-left py-10 px-10 bg-background_secondary 
+        min-h-[1000px] font-[family-name:var(--font-alatsi)]">
+            {pageContent}
         </div>
     );
 }
