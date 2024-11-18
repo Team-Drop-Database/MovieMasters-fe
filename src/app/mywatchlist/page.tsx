d7a847fdbf6e419e88b795dfc70ff873e3f2144b
Thomas Linssen <t.linssen@live.nl>
2024-11-18 18:07:21 +0100

    Added important comment

--- src/app/mywatchlist/page.tsx
+++ src/app/mywatchlist/page.tsx
@@ -22,6 +22,7 @@ export default async function MyWatchList() {
     // Retrieve this user's watchlist, containing all the movies that he's added
     let watchlist: WatchlistItem[];
     try{
+        //TODO: Change this hardcoded user ID of '1' to the logged in user's ID
         const response = await fetch('http://localhost:8080/user/watchlist?userId=1');
          watchlist = await response.json();
     }catch (error: unknown) {
