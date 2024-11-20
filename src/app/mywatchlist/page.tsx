b34007921ffd262636d3da599afb758609c7c1ed
Thomas Linssen <thomas.linssen@hva.nl>
2024-11-20 16:42:54 +0100

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Mitchell Tol <mitchell.tol@hva.nl>

--- src/app/mywatchlist/page.tsx
+++ src/app/mywatchlist/page.tsx
@@ -52,8 +52,7 @@ export default async function MyWatchList() {
                 <h3 className="text-l mt-2 text-center">{item.movie.title}</h3>
 
                 {/* Show your rating as well if you've watched it */}
-                {item.watched ? <h5 
-                className="text-sm opacity-50">You rated it: 3.4 Stars</h5> : <div></div>}
+                { item.watched && <h5 className="text-sm opacity-50">You rated it: 3.4 Stars</h5>}
                 
             </div>);
     }
