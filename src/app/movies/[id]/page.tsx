a91c2c0cb85706deef98364d7907506a3345f89c
Mark Reijn <mark.reijn@hva.nl>
2024-11-23 09:22:17 +0100

    Added page to see details about a movie

---
+++ src/app/movies/[id]/page.tsx
@@ -0,0 +1,48 @@
+﻿import Image from "next/image";
+import { Movie } from "@/interfaces/movie";
+
+export default async function Movies({params}: {
+    params: Promise<{id: number}>
+}) {
+    const { id } = await params;
+    const movieId: number = id;
+
+    if (isNaN(movieId)) {
+        return <div>The movie ID must be a number.</div>;
+    }
+
+    let response: Response;
+    let movie: Movie;
+    try {
+        response = await fetch(`${process.env.DATABASE_HOST_URL}/api/v1/movies/${movieId}`);
+
+        switch(response.status) {
+            case 200: {
+                movie = await response.json();
+                break;
+            }
+            case 404: {
+                return <div>Movie not found.</div>;
+            }
+            default: {
+                return <div>An error occurred while trying to retrieve the movie.</div>;
+            }
+        }
+    } catch (error) {
+        console.log(error);
+        return <div>An error occurred while trying to retrieve the movie.</div>;
+    }
+
+    return (
+        <div className="flex mx-10 my-5 space-x-20">
+            <div className="">
+                <div className="text-4xl">{movie.title}</div>
+                <Image src={movie.posterPath} alt={movie.title} width="300" height="300"/>
+            </div>
+            <div className="">
+                <div className="text-2xl">Description</div>
+                <p>{movie.description}</p>
+            </div>
+        </div>
+    );
+}
---
+++ src/interfaces/movie.ts
@@ -0,0 +1,9 @@
+﻿export interface Movie {
+    id: number;
+    title: string;
+    description: string;
+    language: string;
+    releaseDate: Date;
+    rating: number;
+    posterPath: string;
+}
