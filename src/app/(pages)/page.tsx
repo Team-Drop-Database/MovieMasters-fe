da0b687041c1ad039f6d629d77b59e9261709d47
Mitchell Tol <mitchell.tol@hva.nl>
2024-11-25 23:26:25 +0100

    Home page

---
+++ src/app/(pages)/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+import React from "react";
+import LoggedOut from "./LoggedOut";
+import LoggedIn from "./LoggedIn";
+
+export default function Home() {
+  const [loggedIn, setLoggedIn] = React.useState(false)
+
+  return (
+    <main className="flex flex-col gap-14 font-[family-name:var(--font-alatsi)]">
+      { loggedIn ? <LoggedIn /> : <LoggedOut /> }
+    </main>
+  );
+}
+
