f1aa937ed483b91a2991927b5f28a584119a1ae7
Mark Reijn <mark.reijn@hva.nl>
2024-11-28 14:28:33 +0100

    Changes

--- src/models/Movie.ts
+++ src/models/Movie.ts
@@ -2,13 +2,13 @@
  * Represents a Movie object.
  */
 export type Movie = {
-    id: number;
-    title: string;
-    description: string;
-    language: string;
-    releaseDate: Date;
-    tmdbRating: number;
-    posterPath: string;
+  id: number;
+  title: string;
+  description: string;
+  language: string;
+  releaseDate: Date;
+  tmdbRating: number;
+  posterPath: string;
 }
 
 export default Movie;
--- src/services/MovieService.ts
+++ src/services/MovieService.ts
@@ -1,26 +1,25 @@
-﻿import Movie from "@/app/model/movie";
+﻿import Movie from "@/models/Movie"
 
 export default async function getMovieById(movieId: number): Promise<Movie | undefined> {
-    try {
-        const response: Response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}${process.env.NEXT_PUBLIC_API_VERSION}/movies/${movieId}`);
+  try {
+    const response: Response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}${process.env.NEXT_PUBLIC_API_VERSION}/movies/${movieId}`);
 
-        let movie: Movie;
-        switch(response.status) {
-            case 200: {
-                movie = await response.json();
-                return movie;
-            }
-            case 404: {
-                console.error("Could not find movie with ID: " + movieId);
-                break;
-            }
-            default: {
-                console.error("Something went wrong while trying to get the movie with ID: " + movieId);
-                break;
-            }
-        }
-    } catch (error: unknown) {
-        console.log(error);
-        throw new Error("An error occurred while trying to retrieve the movie.");
+    let movie: Movie;
+    switch(response.status) {
+      case 200: {
+        movie = await response.json();
+        return movie;
+      }
+      case 404: {
+        console.error("Could not find movie with ID: " + movieId);
+        break;
+      }
+      default: {
+        console.error("Something went wrong while trying to get the movie with ID: " + movieId);
+        break;
+      }
     }
+  } catch (error: unknown) {
+    console.error("An error occurred while trying to retrieve the movie.");
+  }
 }
