6bad4bde6cefd63e7316c4cf58f019619980ee22
Mark Reijn <mark.reijn@hva.nl>
2024-11-28 14:29:09 +0100

    Indenting

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    images: {
-        remotePatterns: [{
-            protocol: "https",
-            hostname: "image.tmdb.org",
-        }]
-    }
+  images: {
+    remotePatterns: [{
+      protocol: "https",
+      hostname: "image.tmdb.org",
+    }]
+  }
 };
 
 export default nextConfig;
