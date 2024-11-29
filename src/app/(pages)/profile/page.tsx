c2438f5596e284784fc61e48bd01673e1d5763dc
Lorenzo <lorenzo_veld@hotmail.com>
2024-11-29 16:37:54 +0100

    Added button

--- src/app/(pages)/profile/page.tsx
+++ src/app/(pages)/profile/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import {Button} from "@/components/generic/Button";
+
 export default function Profile() {
   const profilePictureURL = "https://lumiere-a.akamaihd.net/v1/images/r2-d2-main_f315b094.jpeg?" +
     "region=273%2C0%2C951%2C536"; // TODO: Make this so it takes the url from the user.
@@ -5,19 +9,27 @@ export default function Profile() {
   return (
     <div className="flex justify-center">
       <div className="w-full max-w-sm p-6 rounded-lg mt-6">
+        <div className="flex justify-center items-center">
+          <img
+            src={profilePictureURL}
+            alt="Profile picture"
+            className="w-40 h-40 object-cover rounded-full"
+          />
+        </div>
         {/* Using img over Image because for Image a config would be needed for all domains */}
         {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          src={profilePictureURL}
-          alt="Profile picture"
-          className="w-24 h-24 object-cover rounded-full"
-        />
-        <p>
+        <p className="m-2">
           Username
         </p>
-        <p>
+        <p className="m-2">
           E-mail
         </p>
+        <div className="m-10">
+          <Button
+            text="Edit"
+            onClick={() => console.log("Change to textfields")}
+          />
+        </div>
       </div>
     </div>
   )
