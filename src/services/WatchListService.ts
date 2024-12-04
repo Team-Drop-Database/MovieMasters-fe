7a8fe417e967e8616b1faa95299aa146b12a3ec4
Thomas Linssen <t.linssen@live.nl>
2024-12-04 12:14:14 +0100

    Added service functionality for retrieving the watchedStatus

--- src/services/WatchListService.ts
+++ src/services/WatchListService.ts
@@ -1,9 +1,19 @@
 import WatchlistItem from "@/models/WatchListItem";
 
+/**
+ * API Service containing async methods for 
+ * watchlist-related database operations.
+ */
+
+/**
+ * Enum representing the current 
+ * state of a user and a movie.
+ */
 export enum WatchedState {
-    WATCHED,
-    UNWATCHED,
-    ERROR
+    NOT_WATCHLISTED = 'NOT_WATCHLISTED',
+    WATCHED = 'WATCHED',
+    UNWATCHED = 'UNWATCHED',
+    ERROR = 'ERROR'
 };
 
 /**
@@ -15,7 +25,8 @@ export enum WatchedState {
  * of WatchlistItems
  */
 export async function retrieveWatchlistByUser(userId: number): Promise<WatchlistItem[]> {
-    const QUERY_URL = `${process.env.NEXT_PUBLIC_API_BASE_URL}${process.env.NEXT_PUBLIC_API_VERSION}/users/${userId}/watchlist`;
+    const QUERY_URL = `${process.env.NEXT_PUBLIC_API_BASE_URL}${process
+        .env.NEXT_PUBLIC_API_VERSION}/users/${userId}/watchlist`;
 
     try {
         const response = await fetch(QUERY_URL);
@@ -28,7 +39,16 @@ export async function retrieveWatchlistByUser(userId: number): Promise<Watchlist
     }
 }
 
-export async function hasWatched(userId: number, movieId: number): Promise<WatchedState> {
+/**
+ * Retrieve the state of a users relation with a movie.
+ * This is in 'WatchedState' form.
+ * 
+ * @param userId id of a user
+ * @param movieId id of a movie
+ * @returns Promise containing the state of whether
+ * the user has a movie on his watchlist
+ */
+export async function getWatchedStatus(userId: number, movieId: number): Promise<WatchedState> {
 
     // Fetch the watchlist first
     let watchlist: WatchlistItem[];
@@ -36,7 +56,7 @@ export async function hasWatched(userId: number, movieId: number): Promise<Watch
         watchlist = await retrieveWatchlistByUser(userId);
     } catch(error: unknown) {
         if (error instanceof Error)
-            console.error(`Failed to lookup 'hasWatched' data 
+            console.error(`Failed to lookup 'getWatchedStatus' data 
         for user userId '${userId}' and movieId '${movieId
             }'.\nError message: ${error.message}.`);
         return WatchedState.ERROR;
@@ -45,9 +65,19 @@ export async function hasWatched(userId: number, movieId: number): Promise<Watch
     // Filter on the movieId, check whether its in the list of movies
     const includesMovie = watchlist
         .filter((item) => item.movie.id == movieId);
-    const watched = includesMovie.length > 0 ? 
+
+    // Is it on his watchlist? If not, return that
+    const watchListed = includesMovie.length > 0;
+    if(!watchListed)
+        return WatchedState.NOT_WATCHLISTED;
+
+    // Otherwise, take the data and check his watched status
+    const movieAssociation = includesMovie[0];    
+    const hasWatched = movieAssociation.watched ? 
         WatchedState.WATCHED : WatchedState.UNWATCHED;
 
     // Return the result
-    return watched;
+    return hasWatched;
+}
+
 }
