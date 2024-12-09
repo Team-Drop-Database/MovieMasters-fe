d71ec10c55fa3f9073c3f4909262c0c450ddbcfe
Thomas Linssen <t.linssen@live.nl>
2024-12-09 11:55:14 +0100

    Solved watchlist issue

--- src/app/(pages)/mywatchlist/page.tsx
+++ src/app/(pages)/mywatchlist/page.tsx
@@ -65,71 +65,6 @@ export default function MyWatchList() {
         ratingMessage = null;
       }
 
-<<<<<<< HEAD
-            // Construct message about what the user has rated
-            let ratingMessage;
-            if (item.watched && item.review) {
-                ratingMessage = <h5 className="text-xs opacity-50">You rated it: {item.review.rating} Stars</h5>;
-            } else if (item.watched) {
-                ratingMessage = <h5 className="text-xs opacity-50 w-[80%] text-center 
-                text-blue-500 hover:underline">Review this movie</h5>;
-            } else {
-                ratingMessage = '';
-            }
-
-            // Return the movie widget including information such as the 
-            // description, TMDB rating and possibly the users' own rating
-            return <Link className="flex flex-col items-center max-w-[185px] cursor-pointer"
-             key={index} href={`/movies/${item.movie.id}`}>
-                <div className=" w-full h-full relative group">
-                    <img className="object-cover w-[185px] aspect-[2/3]" src={item.movie.posterPath} 
-                    alt={`${item.movie.title}_thumbnail.png`}/>
-                    <div className="group-hover:opacity-75 transition-opacity opacity-0 absolute
-                     top-0 left-0 bg-black h-full w-full flex justify-evenly items-center flex-col">
-                        <p className="text-center text-sm w-[90%]">{description}</p>
-                        <p className="text-center text-sm">TMDB Rating: <span 
-                            className="text-blue-500">{item.movie.tmdbRating}</span></p>
-                    </div>
-                </div>
-                <h3 className="text-l mt-2 text-center">{item.movie.title}</h3>
-
-                {/* Show your rating as well if you've watched it (and reviewed it)*/}
-                { ratingMessage }
-
-            </Link>});
-    }
-
-    // If page content is empty, show it on the screen. Otherwise, show the lists of movies.
-    let pageContent;
-    if(watchlist.length === 0) {
-        pageContent = 
-        <div>
-            <h2 className="text-3xl">Still empty here...</h2>
-            <p>Add some movies to make them show up here!</p>
-        </div>
-    }
-    else {
-        // Display both the watched and unwatched movies in separate sections
-        pageContent = (
-            <ElementTransition>
-                <div>
-                    <div className="p-4">
-                        <h1 className="text-2xl">Watched</h1>
-                        <div className="flex gap-5 mt-2 py-4 items-start flex-wrap">
-                            {mapMoviesToList(watchedMovies)}
-                        </div>
-                    </div>
-                    <div className="p-4">
-                        <h1 className="text-2xl">Plan to watch</h1>
-                        <div className="flex gap-5 mt-2 py-4 items-start flex-wrap">
-                            {mapMoviesToList(planToWatchMovies)}
-                        </div>
-                    </div>
-                </div>
-            </ElementTransition>
-        )
-    }
-=======
       return (
         <Link
           className="flex flex-col items-center max-w-[185px] cursor-pointer"
@@ -159,7 +94,6 @@ export default function MyWatchList() {
       );
     });
   };
->>>>>>> main
 
   if (error) {
     return <p>Failed to fetch data. Error: {error}</p>;
@@ -175,20 +109,22 @@ export default function MyWatchList() {
     );
   } else {
     pageContent = (
-      <div>
-        <div className="p-4">
-          <h1 className="text-2xl">Watched</h1>
-          <div className="flex gap-5 mt-2 py-4 items-start flex-wrap">
-            {mapMoviesToList(watchedMovies)}
+      <ElementTransition>
+        <div>
+          <div className="p-4">
+            <h1 className="text-2xl">Watched</h1>
+            <div className="flex gap-5 mt-2 py-4 items-start flex-wrap">
+              {mapMoviesToList(watchedMovies)}
+            </div>
+          </div>
+          <div className="p-4">
+            <h1 className="text-2xl">Plan to watch</h1>
+            <div className="flex gap-5 mt-2 py-4 items-start flex-wrap">
+              {mapMoviesToList(planToWatchMovies)}
+            </div>
           </div>
         </div>
-        <div className="p-4">
-          <h1 className="text-2xl">Plan to watch</h1>
-          <div className="flex gap-5 mt-2 py-4 items-start flex-wrap">
-            {mapMoviesToList(planToWatchMovies)}
-          </div>
-        </div>
-      </div>
+      </ElementTransition>
     );
   }
 
