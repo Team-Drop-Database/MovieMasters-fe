472250b84e54a046392bfe435ab7d66bb3eda631
Thomas Linssen <t.linssen@live.nl>
2024-12-18 12:27:28 +0100

    Changed layout of searchbar and buttons on the right, also added 'Movie Master' logo text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,36 +10,41 @@ export default function Header() {
 
   return (
     <header className="p-5 w-full flex items-center bg-primary shadow-lg font-[family-name:var(--font-alatsi)]">
-      <div className="flex grow items-center gap-[15rem]">
-        <BasicTransitionLink href={"/"}>
-          <Image 
-            src={logo}
-            width={69}
-            height={69}
-            alt="logo"
-            className="rounded-md shadow-md cursor-pointer"
-            onClick={console.log}
-          />
-        </BasicTransitionLink>
-        <SearchBar className="w-1/2" />
-      </div>
-      {isLoggedIn ? (
-        <div className="flex gap-5 items-center">
+      <div className="flex grow items-center">
+        <div className="basis-[30%]">
+          <BasicTransitionLink href={"/"}>
+            <div className="flex items-center gap-2 group">
+              <Image 
+                  src={logo}
+                  width={69}
+                  height={69}
+                  alt="logo"
+                  className="rounded-md cursor-pointer"
+                  onClick={console.log}
+                />
+                <h1 className="font-opensans font-bold pt-2 text-3xl"><span className="text-blue-600 group-hover:text-indigo-600 transition-all duration-300">Movie</span> Master</h1>
+            </div>
+          </BasicTransitionLink>
+        </div>
+        <SearchBar className="basis-[30%] mx-auto" />
+        {isLoggedIn ? (
+        <div className="flex gap-5 items-center basis-[30%] justify-end pr-5">
           <BasicTransitionLink href={"/mywatchlist"}>
             <div className="py-2 px-3 bg-blue-800 rounded-md text-xl">My Watchlist</div>
           </BasicTransitionLink>
           <ProfileButton username={userDetails?.username} />
         </div>
       ) : (
-        <div className="flex gap-5">
+        <div className="flex gap-5 basis-[30%] border border-red-500 justify-end pr-5">
           <BasicTransitionLink href={"/signup"}>
-          <div className="py-2 px-3 bg-blue-800 rounded-md text-xl">Sign up</div>
+          <div className="py-2 px-3 bg-blue-800 rounded-md text-xl hover:scale-110 transition-all hover:bg-indigo-700 hover:opacity-100 hover:ring-1">Sign up</div>
           </BasicTransitionLink>
           <BasicTransitionLink href={"/signin"}>
-          <div className="py-2 px-3 bg-blue-800 rounded-md text-xl">Log in</div>
+          <div className="py-2 px-3 bg-blue-800 rounded-md text-xl  hover:scale-110 transition-all hover:bg-indigo-700 hover:opacity-100 hover:ring-1">Log in</div>
           </BasicTransitionLink>
         </div>
       )}
+      </div>
     </header>
   );
 }
@@ -54,13 +59,13 @@ function SearchBar(props: SearchBarProps) {
   return (
     <input
       value={searchInput}
-      placeholder="Search movies"
+      placeholder="Search for a movie"
       type="text"
       onKeyDown={(e) => {
         if (e.key === "Enter") onConfirmSearch(/*searchInput*/);
       }}
       onChange={(e) => setSearchInput(e.target.value)}
-      className={`${props.className} outline-none placeholder-black py-1 px-2 h-fit rounded-md text-black bg-light_grey hover:bg-light_grey_active duration-300 hover:duration-300 font-[family-name:var(--font-jura)]`}
+      className={`${props.className} outline-none placeholder-black py-2 px-4 h-fit rounded-md text-black bg-light_grey hover:bg-light_grey_active duration-300 hover:duration-300 font-[family-name:var(--font-jura)]`}
     />
   );
 }
