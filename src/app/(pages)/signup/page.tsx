fc02784ae87074012137052142949680bba9d076
Ervin Bošković <ervin.boskovic@hva.nl>
2024-12-20 12:11:42 +0100

    rebrand(register page): rebranded register page

--- src/app/(pages)/signup/page.tsx
+++ src/app/(pages)/signup/page.tsx
@@ -3,11 +3,26 @@
 import {FormEvent, useRef, useState} from "react";
 import Link from "next/link";
 import {Button} from "@/components/generic/Button";
+import {loginUser} from "@/services/AuthService";
+import {useRouter} from "next/navigation";
+import {useAuthContext} from "@/contexts/AuthContext";
 
 export default function Page() {
+  const STATUS_CREATED = 201;
+
   const formRef = useRef<HTMLFormElement | null>(null);
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  const [formErrors, setFormErrors] = useState({
+    email: "",
+    username: "",
+    password: ""
+  });
+  const [errorMessage, setErrorMessage] = useState<string | null>();
+
   const [successMessage, setSuccessMessage] = useState<string | null>(null);
+
+  const { login } = useAuthContext();
+  const router = useRouter();
 
   async function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -16,35 +31,51 @@ export default function Page() {
     const email = formData.get("email") as string;
     const username = formData.get("username") as string;
     const password = formData.get("password") as string;
-    const repeatPassword = formData.get("confirmPassword") as string;
+    const confirmPassword = formData.get("confirmPassword") as string;
 
-    // Check if passwords match
-    if (password !== repeatPassword) {
-      setErrorMessage("Passwords do not match!");
+    if (password !== confirmPassword) {
+      setFormErrors({email: "", username: "", password: "Passwords do not match!"});
       return;
     }
 
-    const payload = {email, username, password};
+    const user = {email, username, password};
 
     try {
       setErrorMessage(null);
+
       const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}${process.env.NEXT_PUBLIC_API_VERSION}/users`, {
-        headers: {"Content-Type": "application/json"},
+        headers: {
+          "Content-Type": "application/json"
+        },
         method: "POST",
-        body: JSON.stringify(payload),
+        body: JSON.stringify(user),
       });
 
-      if (!response.ok) {
-        const errorMessage = await response.text();
-        throw new Error(errorMessage);
+      if (response.status === STATUS_CREATED) {
+        setFormErrors({email: "", password: "", username: ""});
+        setSuccessMessage("Your account has been created");
+        await loginUser(user.username, user.password);
+        await login();
+        router.push("/");
+        return;
       }
 
-      const result = await response.json();
-      setSuccessMessage("User created successfully!");
-      console.log("Response:", result);
+      const errorMessage = await response.text();
+
+      if (errorMessage.startsWith("Email")) {
+        console.error(errorMessage);
+        setFormErrors({email: errorMessage, username: "", password: ""});
+        return;
+      }
+
+      if (errorMessage.startsWith("Username")) {
+        console.error(errorMessage);
+        setFormErrors({email: "", username: errorMessage, password: ""});
+        return;
+      }
     } catch (error) {
-      console.error("Network error:", error);
-      setErrorMessage("An unkown error occured: " + (error as Error).message);
+      console.error(error);
+      setErrorMessage((error as Error).message);
     }
   }
 
@@ -52,72 +83,75 @@ export default function Page() {
     <div className="flex justify-center mb-[480px]">
       <div className="w-full max-w-sm p-6 rounded-lg mt-6">
         <h1 className="text-4xl font-bold text-center mb-6">Sign up!</h1>
-        <form onSubmit={handleSubmit} className="flex flex-col gap-2" ref={formRef}>
-          <label className="font-bold" htmlFor="email">Email</label>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            placeholder="E-mail"
-            required
-            className="outline-none placeholder-black py-1 px-2 h-fit rounded-md text-black bg-light_grey
+        <div className="bg-background_primary w-auto p-5 rounded shadow-lg">
+          <form onSubmit={handleSubmit} className="flex flex-col gap-2" ref={formRef}>
+            <label className="font-bold" htmlFor="email">Email</label>
+            <input
+                id="email"
+                type="email"
+                name="email"
+                placeholder="Enter Email"
+                required
+                className="outline-none placeholder-black py-1 px-2 h-fit rounded-md text-black bg-light_grey
                          hover:bg-light_grey_active hover:duration-300 hover:cursor-text
                          font-[family-name:var(--font-jura)]"
-          />
-          <label className="font-bold" htmlFor="username">Username</label>
-          <input
-            id="username"
-            name="username"
-            placeholder="Username"
-            minLength={5}
-            required
-            className="outline-none placeholder-black py-1 px-2 h-fit rounded-md text-black bg-light_grey
+            />
+            {formErrors.email !== "" && <p className="font-[family-name:var(--font-jura)] text-base text-red-600">{formErrors.email}</p>}
+            <label className="font-bold" htmlFor="username">Username</label>
+            <input
+                id="username"
+                name="username"
+                placeholder="Enter Username"
+                minLength={5}
+                required
+                className="outline-none placeholder-black py-1 px-2 h-fit rounded-md text-black bg-light_grey
                          hover:bg-light_grey_active hover:duration-300 hover:cursor-text
                          font-[family-name:var(--font-jura)]"
-          />
-          <label className="font-bold" htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            name="password"
-            placeholder="Password"
-            minLength={8}
-            required
-            className="outline-none placeholder-black py-1 px-2 h-fit rounded-md text-black bg-light_grey
+            />
+            {formErrors.username !== "" && <p className="font-[family-name:var(--font-jura)] text-base text-red-600">{formErrors.username}</p>}
+            <label className="font-bold" htmlFor="password">Password</label>
+            <input
+                id="password"
+                type="password"
+                name="password"
+                placeholder="Create Password"
+                minLength={8}
+                required
+                className="outline-none placeholder-black py-1 px-2 h-fit rounded-md text-black bg-light_grey
                          hover:bg-light_grey_active hover:duration-300 hover:cursor-text
                          font-[family-name:var(--font-jura)]"
-          />
-          <label className="font-bold" htmlFor="confirmPassword">Confirm Password</label>
-          <input
-            id="confirmPassword"
-            type="password"
-            name="confirmPassword"
-            placeholder="Confirm password"
-            required
-            className="outline-none placeholder-black py-1 px-2 h-fit rounded-md text-black bg-light_grey
+            />
+            <label className="font-bold" htmlFor="confirmPassword">Confirm Password</label>
+            <input
+                id="confirmPassword"
+                type="password"
+                name="confirmPassword"
+                placeholder="Confirm Password"
+                required
+                className="outline-none placeholder-black py-1 px-2 h-fit rounded-md text-black bg-light_grey
                          hover:bg-light_grey_active hover:duration-300 hover:cursor-text
                          font-[family-name:var(--font-jura)]"
-          />
-
-          <div className="text-center mt-4 mb-1">
-            <p className="text-sm">
-              Already have an account?{" "}
-              <Link
-                href="/signin"
-                className="underline hover:cursor-pointer hover:no-underline"
-              >
-                Sign in here!
-              </Link>
-            </p>
-          </div>
-
-          <Button
-            text="Create Account"
-            onClick={() => formRef.current?.requestSubmit()}
-          />
-        </form>
-        {errorMessage && (<p className="text-red-600 font-medium mt-4 text-center">{errorMessage}</p>)}
-        {successMessage && (<p className="text-green-600 font-medium mt-4 text-center">{successMessage}</p>)}
+            />
+            {formErrors.password !== "" && <p className="font-[family-name:var(--font-jura)] text-base text-red-600">{formErrors.password}</p>}
+            {errorMessage && <p className="font-[family-name:var(--font-jura)] text-base text-red-600">{errorMessage}</p>}
+            {successMessage && <p className="font-[family-name:var(--font-jura)] text-base text-green-600">{successMessage}</p>}
+            <div className="text-center mt-4 mb-1">
+              <p className="text-sm">
+                Already have an account?{" "}
+                <Link
+                    href="/signin"
+                    className="underline hover:cursor-pointer hover:no-underline"
+                >
+                  Sign in here!
+                </Link>
+              </p>
+            </div>
+            <Button
+                text="Create Account"
+                onClick={() => formRef.current?.requestSubmit()}
+            />
+          </form>
+        </div>
       </div>
     </div>
   );
