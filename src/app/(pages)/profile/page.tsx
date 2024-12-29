95cdc5fd9c6e2921460ac6cbf1f55c86a3bdd8d6
Thomas Tol <thomas.tol@hva.nl>
2024-12-29 14:47:47 +0100

    Fix/profile-picture

--- src/app/(pages)/profile/page.tsx
+++ src/app/(pages)/profile/page.tsx
@@ -1,17 +1,18 @@
 "use client";
 
-import {Button} from "@/components/generic/Button";
-import {ChangeEvent, useEffect, useState} from "react";
-import {useAuthContext} from "@/contexts/AuthContext";
-import {navigateToLogin} from "@/utils/navigation/HomeNavigation";
+import { Button } from "@/components/generic/Button";
+import { ChangeEvent, useEffect, useState } from "react";
+import { useAuthContext } from "@/contexts/AuthContext";
+import { navigateToLogin } from "@/utils/navigation/HomeNavigation";
 import Loading from "@/components/generic/Loading";
 import Cookies from "js-cookie";
 import { useRouter } from "next/navigation";
-import {fetchUserData, updateUser} from "@/services/UserService";
+import {fetchUserData, updateUser, uploadImageToImgbb} from "@/services/UserService";
+import neutral from "@/assets/images/no-profile-pic.jpg"
 
 export default function Profile() {
   const profile = {
-    profilePictureURL: "",
+    profilePictureUrl: "",
     username: "",
     email: ""
   }
@@ -22,8 +23,10 @@ export default function Profile() {
   const [originalData, setOriginalData] = useState(profile);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const [selectedFile, setSelectedFile] = useState<File | null>(null); // To hold the file object
   const router = useRouter();
-  const JWT_COOKIE_SECURE: boolean = process.env.JWT_COOKIE_SECURE?.toLowerCase() == 'true';
+  const JWT_COOKIE_SECURE: boolean =
+    process.env.JWT_COOKIE_SECURE?.toLowerCase() === "true";
 
   useEffect(() => {
     if (!isLoggedIn) {
@@ -37,12 +40,10 @@ export default function Profile() {
           const initialData = {
             username: userData.username,
             email: userData.email,
-            profilePictureURL: userData.profile_picture || "https://static.vecteezy.com/system" +
-              "/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg",
-            //TODO: issue #97 (make sure there is already image in database)
+            profilePictureUrl: userData.profile_picture || neutral.src
           };
           setProfileData(initialData);
-          setOriginalData(initialData); // This is to make sure that when you cancel editting you get the original data back
+          setOriginalData(initialData);
         } catch (error) {
           if (error instanceof Error) {
             console.error(error.message);
@@ -65,14 +66,31 @@ export default function Profile() {
     return <div>Error: {error}</div>
   }
 
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const {name, value} = e.target;
-    setProfileData((prevData) => ({...prevData, [name]: value}));
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      // Check file size limit
+      if (file.size > 1.4 * 1024 * 1024) { // 1.4MB limit
+        alert("File size must be less than 1.4MB.");
+        return;
+      }
+      setSelectedFile(file);
+      // Update profile picture preview
+      setProfileData((prevData) => ({
+        ...prevData,
+        profilePictureUrl: URL.createObjectURL(file),
+      }));
+    }
+  };
 
-    if ((name === "username" && value.length < 5)) {
-      setIsSaveDisabled(true)
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setProfileData((prevData) => ({ ...prevData, [name]: value }));
+
+    if (name === "username" && value.length < 5) {
+      setIsSaveDisabled(true);
     } else {
-      const isInvalid = (name !== "username" && profileData.username.length < 5);
+      const isInvalid = name !== "username" && profileData.username.length < 5;
       setIsSaveDisabled(isInvalid);
     }
   };
@@ -86,22 +104,38 @@ export default function Profile() {
 
       if (userDetails != null) {
         try {
+          let profilePictureUrl = profileData.profilePictureUrl;
+
+          // If a new file is selected, upload it
+          if (selectedFile) {
+            const uploadedUrl = await uploadImageToImgbb(selectedFile);
+            if (uploadedUrl) {
+              profilePictureUrl = uploadedUrl;
+            }
+          }
+
           const tokens = await updateUser(userDetails.userId, {
             username: profileData.username,
             email: profileData.email,
-            profilePicture: profileData.profilePictureURL
+            profilePicture: profilePictureUrl,
           });
 
           if (tokens) {
-            Cookies.set("jwt", tokens.accessToken, {expires: 1, secure: JWT_COOKIE_SECURE,
-              sameSite: "Strict"});
-            Cookies.set("refresh_token", tokens.refreshToken,
-              {expires: 3, secure: JWT_COOKIE_SECURE, sameSite: "strict"});
+            Cookies.set("jwt", tokens.accessToken, {
+              expires: 1,
+              secure: JWT_COOKIE_SECURE,
+              sameSite: "Strict",
+            });
+            Cookies.set("refresh_token", tokens.refreshToken, {
+              expires: 3,
+              secure: JWT_COOKIE_SECURE,
+              sameSite: "strict",
+            });
             await login();
             router.push("/");
           }
 
-          setOriginalData(profileData);
+          setOriginalData({ ...profileData, profilePictureUrl: profilePictureUrl });
         } catch (error) {
           if (error instanceof Error) {
             console.error("Error updating profile: ", error.message);
@@ -117,71 +151,61 @@ export default function Profile() {
   const cancelEdit = () => {
     setProfileData(originalData);
     setIsEditing(false);
-  }
+    setSelectedFile(null);
+  };
 
   return (
     <div className="flex justify-center">
       <div className="w-full max-w-sm p-6 rounded-lg mt-6 bg-background_secondary">
-        <div className="flex justify-center items-center">
-          <img
-            src={profileData.profilePictureURL}
-            alt="Profile picture"
-            className="w-40 h-40 object-cover rounded-full"
+        <div className="flex justify-center items-center relative group">
+          <div className="relative">
+            <img
+              src={profileData.profilePictureUrl}
+              alt="Profile picture"
+              className="w-40 h-40 object-cover rounded-full"
+            />
+            {isEditing && (
+              <div
+                className="absolute inset-0 bg-black bg-opacity-50 rounded-full flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 cursor-pointer"
+                onClick={() => document.getElementById("file-input")?.click()}
+              >
+                <span className="text-white font-medium text-md">Edit</span>
+              </div>
+            )}
+          </div>
+          <input
+            type="file"
+            id="file-input"
+            accept="image/*"
+            className="hidden"
+            onChange={handleFileChange}
           />
         </div>
+
         {isEditing ? (
           <div className="m-2">
-            <label className="block text-m font-medium">
-              Profile Picture URL
-            </label>
-            <input
-              type="text"
-              name="profilePictureURL"
-              value={profileData.profilePictureURL}
-              onChange={handleInputChange}
-              className="outline-none placeholder-black py-1 px-2 h-fit rounded-md text-black bg-light_grey
-                         hover:bg-light_grey_active hover:duration-300 hover:cursor-text
-                         font-[family-name:var(--font-jura)] w-full"
-            />
-            <label className="block text-m font-medium mt-4">
-              Username
-            </label>
+            <label className="block text-m font-medium mt-4">Username</label>
             <input
               type="text"
               name="username"
               minLength={5}
               value={profileData.username}
               onChange={handleInputChange}
-              className="outline-none placeholder-black py-1 px-2 h-fit rounded-md text-black bg-light_grey
-                         hover:bg-light_grey_active hover:duration-300 hover:cursor-text
-                         font-[family-name:var(--font-jura)] w-full"
+              className="outline-none py-1 px-2 h-fit rounded-md bg-light_grey hover:bg-light_grey_active w-full"
             />
-            {profileData.username.length < 5 && (
-              <label className="block text-m text-red-500">
-                Username must be at least 5 characters
-              </label>
-            )}
-            <label className="block text-m font-medium mt-4">
-              E-mail
-            </label>
+            <label className="block text-m font-medium mt-4">E-mail</label>
             <input
               type="email"
               name="email"
               value={profileData.email}
               onChange={handleInputChange}
-              className="outline-none placeholder-black py-1 px-2 h-fit rounded-md text-black bg-light_grey
-                         hover:bg-light_grey_active hover:duration-300 hover:cursor-text
-                         font-[family-name:var(--font-jura)] w-full"
+              className="outline-none py-1 px-2 h-fit rounded-md bg-light_grey hover:bg-light_grey_active w-full"
             />
           </div>
         ) : (
           <div>
-            <p className="m-2 text-center font-medium">
-              {profileData.username}
-            </p>
-            <p className="m-2 text-center font-medium">
-              {profileData.email}
-            </p>
+            <p className="m-2 text-center font-medium">{profileData.username}</p>
+            <p className="m-2 text-center font-medium">{profileData.email}</p>
           </div>
         )}
         <div className="ml-10 mr-10 mt-5">
