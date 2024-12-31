86f8c02177f0b8059d0249d53cde30e9ccf5b607
Lorenzo <lorenzo_veld@hotmail.com>
2024-12-31 13:17:40 +0100

    requests are not shown when empty

--- src/app/(pages)/friends/page.tsx
+++ src/app/(pages)/friends/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import React from "react";
 
 export default function Friends() {
-  // Temporary hardcoded data (can be replaced with API data)
+  // Temporary hardcoded data (will be replaced with API data)
   const friendRequests = [
     {
       username: "JohnDoe",
@@ -33,9 +33,7 @@ export default function Friends() {
 
   return (
     <div className="flex flex-col items-center">
-      {/* Friends and Friend Requests Section */}
       <div className="flex flex-row justify-center space-x-4 mt-6 w-full">
-        {/* Friends Section */}
         <div className="w-2/5 p-6 rounded-lg bg-background_secondary">
           <h1 className="mb-2">Friends</h1>
           {friends.map((friend, index) => (
@@ -51,27 +49,27 @@ export default function Friends() {
             </div>
           ))}
         </div>
-
-        {/* Friend Requests Section */}
-        <div className="w-2/5 p-8 rounded-lg bg-background_secondary">
-          <h1 className="mb-2">Friend Requests</h1>
-          {friendRequests.map((friend, index) => (
-            <div className="flex items-center" key={index}>
-              <div className="flex items-center space-x-4">
-                <img
-                  src={friend.profilePictureUrl}
-                  alt={`${friend.username}'s profile`}
-                  className="w-14 h-14 object-cover rounded-full"
-                />
-                <label className="block text-m font-medium">{friend.username}</label>
+        {friendRequests.length > 0 && (
+          <div className="w-2/5 p-8 rounded-lg bg-background_secondary">
+            <h1 className="mb-2">Friend Requests</h1>
+            {friendRequests.map((friend, index) => (
+              <div className="flex items-center" key={index}>
+                <div className="flex items-center space-x-4">
+                  <img
+                    src={friend.profilePictureUrl}
+                    alt={`${friend.username}'s profile`}
+                    className="w-14 h-14 object-cover rounded-full"
+                  />
+                  <label className="block text-m font-medium">{friend.username}</label>
+                </div>
+                <div className="flex space-x-2 ml-auto">
+                  <Image src="/checkmark.svg" width={30} height={30} alt="checkmark"/>
+                  <Image src="/red-cross.svg" width={35} height={35} alt="red cross"/>
+                </div>
               </div>
-              <div className="flex space-x-2 ml-auto">
-                <Image src="/checkmark.svg" width={30} height={30} alt="checkmark"/>
-                <Image src="/red-cross.svg" width={35} height={35} alt="red cross"/>
-              </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        )}
       </div>
 
       {/* Add Friend Button */}
