0aac59076470ee6dc494e9689f83ee0bb9d72dd4
Ervin Bošković <ervin.boskovic@hva.nl>
2025-01-01 18:48:53 +0100

    page(404): custom generic 404 page added

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <div className="text-center">
+            <h1 className="text-9xl font-[family-name:var(--font-alatsi)">404</h1>
+            <p className="font-[family-name:var(--font-alatsi)">The page that you are looking for does not exist</p>
+        </div>
+    )
+}
