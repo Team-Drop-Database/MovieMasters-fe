c19c5ef6bb6140b4102749f22797ea962529e325
Mark Reijn <mark.reijn.reijn@hva.nl>
2025-01-06 21:13:56 +0100

    Feature/search

---
+++ src/components/generic/search/DisplayMovies.tsx
@@ -0,0 +1,24 @@
+import Movie from "@/models/Movie";
+import {redirect} from "next/navigation";
+
+export default function DisplayMovies({ movies} : { movies: Movie[]}) {
+  return (
+    <div className="grid grid-cols-2 gap-4">
+      {movies.map(movie => (
+        <div key={movie.id} className="flex flex-row gap-4 cursor-pointer"
+             onClick={() => redirect(`/movies/${movie.id}`)}>
+          <img
+            className="max-w-20"
+            src={movie.posterPath}
+            alt={movie.title}
+          />
+          <div className="hover:brightness-50">
+            <h1>{movie.title}</h1>
+            <div>{new Date(movie.releaseDate).getFullYear()}</div>
+            <div>TMDB rating: {movie.tmdbRating}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
