badb0c2042831294a8167362953ab5c05d9ef3cc
Mark Reijn <mark.reijn.reijn@hva.nl>
2025-01-07 20:56:49 +0100

    Fix search related build error

--- src/app/(pages)/search/page.tsx
+++ src/app/(pages)/search/page.tsx
@@ -2,23 +2,25 @@
 import Movie from "@/models/Movie";
 import {getMovieByTitle, getNumberOfPages} from "@/services/MovieService";
 import {useEffect, useRef, useState} from "react";
-//import {useSearchParams} from 'next/navigation'
 import DisplayMovies from "@/components/generic/search/DisplayMovies";
+import {useSearchParams} from "next/navigation";
+import {Suspense} from 'react';
 
-export default function Search() {
-  //const searchParams = useSearchParams();
+function SearchContent() {
+  const searchParams = useSearchParams();
   const [movies, setMovies] = useState<Movie[]>([]);
   const [error, setError] = useState<string | null>(null);
   const [numberOfPages, setNumberOfPages] = useState<number>(0);
   const [pageNumber, setPageNumber] = useState<number>(1);
-  const title: string | null = '';//searchParams.get("title");
-  const prevValues = useRef({ title: title, pageNumber: pageNumber });
+  const title: string | null = searchParams.get("title");
+  const prevValues = useRef({title: title, pageNumber: pageNumber});
 
   // Fetching new movies when the title or page number changes
   useEffect(() => {
-    if (prevValues.current.title != title) {
+    if (prevValues.current.title !== title) {
       setPageNumber(1);
     }
+
     async function setTotalPages() {
       try {
         setNumberOfPages(await getNumberOfPages(title));
@@ -33,7 +35,7 @@ export default function Search() {
 
     async function fetchMovies() {
       try {
-        setMovies(await getMovieByTitle(title, pageNumber-1));
+        setMovies(await getMovieByTitle(title, pageNumber - 1));
       } catch (error: unknown) {
         if (error instanceof Error) {
           setError(error.message);
@@ -42,10 +44,11 @@ export default function Search() {
         }
       }
     }
+
     setTotalPages().then();
     fetchMovies().then();
 
-    prevValues.current = { title, pageNumber };
+    prevValues.current = {title, pageNumber};
   }, [title, pageNumber]);
 
   if (error) {
@@ -57,9 +60,13 @@ export default function Search() {
    *
    * @param number Number to display in the button
    */
-  function PageNumberButton({number} : {number : number}) {
-    return <button className="hover:backdrop-brightness-50 px-3 py-1 rounded-lg"
-    onClick={() => setPageNumber(number)}>{number}</button>
+  function PageNumberButton({number}: { number: number }) {
+    return (
+      <button className="hover:backdrop-brightness-50 px-3 py-1 rounded-lg"
+              onClick={() => setPageNumber(number)}>
+        {number}
+      </button>
+    );
   }
 
   /**
@@ -68,7 +75,11 @@ export default function Search() {
   function PaginationButtons() {
     // Only showing one element if there is one page
     if (numberOfPages === 1) {
-      return <div className="backdrop-brightness-50 px-3 py-1 rounded-lg">{pageNumber}</div>
+      return (
+        <div className="backdrop-brightness-50 px-3 py-1 rounded-lg">
+          {pageNumber}
+        </div>
+      );
     }
 
     // Determine the start number of the first button
@@ -79,7 +90,7 @@ export default function Search() {
     }
     if (pageNumber === numberOfPages) {
       // For when the user is at last page, if the outcome is smaller than 1, set it to 1
-      startNumber = (numberOfPages - 2) < 1 ? 1 : (numberOfPages - 2);
+      startNumber = numberOfPages - 2 < 1 ? 1 : numberOfPages - 2;
     }
     startNumber = startNumber <= 1 ? 1 : startNumber;
     const buttonItems = [];
@@ -88,19 +99,18 @@ export default function Search() {
     const iterations: number = numberOfPages <= 3 ? numberOfPages : 3;
     for (let i = startNumber; i < iterations + startNumber; i++) {
       // Displaying the
-      if (i == pageNumber) {
+      if (i === pageNumber) {
         buttonItems.push(<div key={i} className="backdrop-brightness-50 px-3 py-1 rounded-lg">{i}</div>);
       } else {
-        buttonItems.push(<PageNumberButton key={i} number={i} />);
+        buttonItems.push(<PageNumberButton key={i} number={i}/>);
       }
     }
 
     // Displaying the amount of pages if the user is not at the final page
     if (numberOfPages - pageNumber > 1) {
       buttonItems.push(<div key="dots" className="px-3 py-1">...</div>);
-      buttonItems.push(<PageNumberButton key="last-page" number={numberOfPages} />)
+      buttonItems.push(<PageNumberButton key="last-page" number={numberOfPages}/>);
     }
-
     return buttonItems;
   }
 
@@ -115,25 +125,34 @@ export default function Search() {
                   hover:cursor-pointer disabled:cursor-not-allowed
                   enabled:hover:backdrop-brightness-50
                   disabled:brightness-50"
-                  onClick={() => setPageNumber(pageNumber - 1)}>← Previous
+                  onClick={() => setPageNumber(pageNumber - 1)}>
+                  ← Previous
           </button>
-          <PaginationButtons />
+          <PaginationButtons/>
           <button disabled={pageNumber === numberOfPages}
-                  className="px-3 py-1 rounded-lg
-                  hover:cursor-pointer disabled:cursor-not-allowed
+                  className="px-3 py-1 rounded-lg hover:cursor-pointer disabled:cursor-not-allowed
                   enabled:hover:backdrop-brightness-50
                   disabled:brightness-50"
-                  onClick={() => setPageNumber(pageNumber + 1)}>Next →
+                  onClick={() => setPageNumber(pageNumber + 1)}>
+                  Next →
           </button>
         </div>
       </div>
     );
   } else {
     return (
-        <div className="flex flex-col items-center">
-          <h1>No movies are found.</h1>
-          <div>No movies found with title: {title}</div>
-        </div>
+      <div className="flex flex-col items-center">
+        <h1>No movies are found.</h1>
+        <div>No movies found with title: {title}</div>
+      </div>
     );
   }
+}
+
+export default function Search() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SearchContent />
+    </Suspense>
+  );
 }
