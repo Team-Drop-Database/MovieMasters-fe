2634061a8cbc06d1fb20cf263fd6af0fe6145617
Thomas Linssen <t.linssen@live.nl>
2025-01-09 15:14:29 +0100

    Added element transition effect to trending page

--- src/app/(pages)/LoggedIn.tsx
+++ src/app/(pages)/LoggedIn.tsx
@@ -1,4 +1,5 @@
 import { TitledHorizontalMoviePager } from "@/components/generic/movie/MovieListItem"
+import ElementTransition from "@/components/generic/transitions/ElementTransition";
 import { getTrendingMovies } from "@/services/TmdbService";
 import { MovieListItemProps } from "@/utils/mapper/MovieResponseMaps"
 import React from "react"
@@ -21,8 +22,10 @@ export default function LoggedIn({ userDetails }: LoggedInProps) {
 
   return (
     <div className="flex flex-col py-2">
-      <h1 className="mx-[4rem]">Welcome back {userDetails?.username}</h1>
-      <TitledHorizontalMoviePager title="Trending Movies" movieItems={trendingMovies} />
+      <ElementTransition startYState={50}>
+          <h1 className="mx-[4rem] font-inter font-semibold  underline-offset-[12px] decoration-slate-600 decoration-1">Welcome back, <span className="font-inter text-blue-500">{userDetails?.username}</span>.</h1>
+          <TitledHorizontalMoviePager title="See what's" movieItems={trendingMovies} />
+      </ElementTransition>
     </div>
   )
 }
