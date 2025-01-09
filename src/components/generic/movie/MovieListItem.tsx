a12ccaefac272ad3752c1d009759fa74959e9a4d
Thomas Linssen <t.linssen@live.nl>
2025-01-09 15:17:06 +0100

    Improved the proportions of the trending page images

--- src/components/generic/movie/MovieListItem.tsx
+++ src/components/generic/movie/MovieListItem.tsx
@@ -123,18 +123,9 @@ function HorizontalMoviePager({ movieItems, cssProperties }: HorizontalMoviePage
 
 function MovieListItem({ title, posterUrl }: MovieListItemProps) {
   return (
-    <div className="w-min flex flex-col items-center grow-0 shrink-0">
-      <div className="relative w-[250px] h-[250px]">
-        <Image
-          src={posterUrl}
-          alt={`Poster for ${title}`}
-          fill
-          priority
-          sizes="(width: 250px), (height: 250px)"
-          className="shadow-2xl rounded-2xl object-cover"
-        />
-      </div>
-      <p className="w-fill text-center">{title}</p>
+    <div className=" flex flex-col items-center grow-0 shrink-0">
+      <img src={posterUrl} width={250} className="shadow-2xl" alt={`Poster for ${title}`}></img>
+      <p className="font-inter font-semibold mt-2 w-fill text-center max-w-[250px]">{title}</p>
     </div>
   )
 }
