0aca9474419c5c74a1d82cdf109bc3edd2252622
Thomas Linssen <t.linssen@live.nl>
2025-01-10 11:20:56 +0100

    Styling improvement. When hovering on a trending movie, it scales slightly to give a nice interactive effect.

--- src/components/generic/movie/MovieListItem.tsx
+++ src/components/generic/movie/MovieListItem.tsx
@@ -122,7 +122,7 @@ function HorizontalMoviePager({ movieItems, cssProperties }: HorizontalMoviePage
 
 function MovieListItem({ title, posterUrl }: MovieListItemProps) {
   return (
-    <div className=" flex flex-col items-center grow-0 shrink-0">
+    <div className=" flex flex-col items-center grow-0 shrink-0 hover:scale-105 transition-transform cursor-pointer">
       <img src={posterUrl} width={250} className="shadow-2xl" alt={`Poster for ${title}`}></img>
       <p className="font-inter font-semibold mt-2 w-fill text-center max-w-[250px]">{title}</p>
     </div>
