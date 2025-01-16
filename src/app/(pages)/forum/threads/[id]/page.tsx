60b8afccf759fa54b8b8cca0a0244fb28eafa95a
Thomas Tol <thomas.tol@hva.nl>
2025-01-16 23:39:27 +0100

    Finalized comment section

--- src/app/(pages)/forum/threads/[id]/page.tsx
+++ src/app/(pages)/forum/threads/[id]/page.tsx
@@ -1,25 +1,25 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
-import { useRouter } from "next/navigation"; // For routing
-import { getTopicById, getCommentsByTopicId, sendComment } from "@/services/ForumService";
-import { Topic } from "@/models/Topic";
-import { Comment } from "@/models/Comment";
+import React, {useEffect, useRef, useState} from "react";
+import {useRouter} from "next/navigation";
+import {getTopicById, getCommentsByTopicId, sendComment} from "@/services/ForumService";
+import {Topic} from "@/models/Topic";
+import {Comment} from "@/models/Comment";
 import Loading from "@/components/generic/Loading";
-import { FaPaperPlane } from "react-icons/fa";
+import {FaPaperPlane} from "react-icons/fa";
 import Image from "next/image";
 import defaultProfilePicture from "@/assets/images/no-profile-pic.jpg";
-import { useAuthContext } from "@/contexts/AuthContext";
+import {useAuthContext} from "@/contexts/AuthContext";
 
-export default function Thread({ params }: { params: Promise<{ id: string }> }) {
+export default function Thread({params}: { params: Promise<{ id: string }> }) {
   const [topic, setTopic] = useState<Topic | null>(null);
   const [comments, setComments] = useState<Comment[] | null>([]);
   const [newComment, setNewComment] = useState<string>("");
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const commentsRef = useRef<HTMLDivElement | null>(null);
-  const router = useRouter(); // For navigation
-  const { userDetails } = useAuthContext();
+  const router = useRouter();
+  const {userDetails} = useAuthContext();
 
   useEffect(() => {
     const fetchTopicAndComments = async () => {
@@ -54,8 +54,8 @@ export default function Thread({ params }: { params: Promise<{ id: string }> }) 
     try {
       const newCommentObject = await sendComment(topic.id, newComment);
       setComments((prevComments) => [...(prevComments || []), newCommentObject]);
-      setNewComment(""); // Clear the input field
-      scrollToBottom(); // Scroll to the bottom
+      setNewComment("");
+      scrollToBottom();
     } catch (err) {
       console.error("Failed to send comment:", err);
     }
@@ -63,7 +63,7 @@ export default function Thread({ params }: { params: Promise<{ id: string }> }) 
 
   const handleKeyDown = (event: React.KeyboardEvent) => {
     if (event.key === "Enter" && !event.shiftKey) {
-      event.preventDefault(); // Prevents new lines when pressing Enter
+      event.preventDefault();
       handleSendComment().then(); // Sends the comment when Enter is pressed
     }
   };
@@ -76,31 +76,26 @@ export default function Thread({ params }: { params: Promise<{ id: string }> }) 
   };
 
   useEffect(() => {
-    scrollToBottom(); // Scroll to the bottom when comments are updated
+    scrollToBottom();
   }, [comments]);
 
   if (loading) {
-    return <Loading />;
+    return <Loading/>;
   }
 
   return (
     <div className="flex flex-col items-start pb-10 px-10 font-[family-name:var(--font-alatsi)]">
-      {/* Header */}
       <h1>Thread</h1>
 
-      {/* Main Thread Content */}
       <div className="flex flex-col w-full max-w-4xl mx-auto">
-        {/* Back to Forum */}
         <div
           className="text-gray-300 mb-1 cursor-pointer hover:text-gray-200 hover:duration-300"
-          onClick={() => router.push("/forum")} // Replace "/forum" with the actual forum route
+          onClick={() => router.push("/forum")}
         >
           <span className="mr-2 text-lg">&lt;&lt; Back to forum</span>
         </div>
 
-        {/* Thread Content Div */}
         <div className="bg-background_primary py-5 p-8 rounded-lg shadow-lg">
-          {/* Topic Section */}
           {!topic ? (
             <p>Topic not found.</p>
           ) : error ? (
@@ -108,15 +103,10 @@ export default function Thread({ params }: { params: Promise<{ id: string }> }) 
           ) : (
             <>
               <div className="mb-3">
-                {/* Title */}
                 <h1 className="text-3xl mb-1">{topic.title}</h1>
-
-                {/* Description */}
                 <p className="text-lg mb-3 text-gray-300">{topic.description}</p>
 
-                {/* Profile Section */}
                 <div className="flex items-center space-x-4">
-                  {/* Profile Picture */}
                   <div className="relative w-14 h-14">
                     <Image
                       src={topic.createdByProfilePicture || defaultProfilePicture}
@@ -127,7 +117,6 @@ export default function Thread({ params }: { params: Promise<{ id: string }> }) 
                     />
                   </div>
 
-                  {/* Name and Date */}
                   <div>
                     <p className="text-[1.1rem] text-gray-400">
                       {topic.createdByUsername}
@@ -139,10 +128,9 @@ export default function Thread({ params }: { params: Promise<{ id: string }> }) 
                 </div>
               </div>
 
-              {/* Comments Section */}
               <div
                 ref={commentsRef}
-                className="flex flex-col space-y-4 max-h-[15rem] min-h-[15rem] overflow-y-auto scrollbar-hide"
+                className="flex flex-col space-y-4 max-h-[15rem] min-h-[15rem] p-8 overflow-y-auto scrollbar-hide shadow-md"
               >
                 {comments && comments.length > 0 ? (
                   comments
@@ -150,9 +138,9 @@ export default function Thread({ params }: { params: Promise<{ id: string }> }) 
                     .map((comment) => (
                       <div
                         key={comment.id}
-                        className="border-t border-gray-300 text-gray-300 p-4 shadow-sm text-sm flex space-x-4"
+                        className="border-t border-gray-400 text-gray-300 pt-4 pb-4 px-2 shadow-sm text-sm flex items-start items-center space-x-4"
                       >
-                        <div className="relative w-12 h-12">
+                        <div className="relative w-12 h-12 self-start">
                           <Image
                             src={comment.profilePicture || defaultProfilePicture}
                             alt={comment.username}
@@ -161,10 +149,14 @@ export default function Thread({ params }: { params: Promise<{ id: string }> }) 
                             sizes="48px"
                           />
                         </div>
-                        <div>
-                          <p className="font-bold">{comment.username}</p>
-                          <p>{comment.content}</p>
-                          <p className="text-xs text-gray-500">{comment.formattedCreationDate}</p>
+
+                        <div className="flex flex-1 flex-col space-y-1">
+                          <div className="flex justify-between items-center">
+                            <p className="text-[0.9rem] text-gray-400">{comment.username}</p>
+                            <p className="text-[0.9rem] text-gray-400">{comment.formattedCreationDate}</p>
+                          </div>
+
+                          <p className="text-[1rem] text-gray-300">{comment.content}</p>
                         </div>
                       </div>
                     ))
@@ -175,9 +167,7 @@ export default function Thread({ params }: { params: Promise<{ id: string }> }) 
                 )}
               </div>
 
-              {/* Add Comment Section */}
               <div className="mt-6 flex items-start space-x-4">
-                {/* User Profile Picture */}
                 <div className="relative w-12 h-12">
                   <Image
                     src={userDetails?.profileUrl || defaultProfilePicture}
@@ -188,7 +178,6 @@ export default function Thread({ params }: { params: Promise<{ id: string }> }) 
                   />
                 </div>
 
-                {/* Input Field and Send Icon */}
                 <div className="flex-grow flex items-center">
                   <input
                     type="text"
@@ -198,7 +187,6 @@ export default function Thread({ params }: { params: Promise<{ id: string }> }) 
                     onKeyDown={handleKeyDown} // Add onKeyDown to handle Enter key
                     className="w-full bg-gray-700 text-gray-300 p-3 rounded-md outline-none hover:bg-gray-600 transition-colors"
                   />
-                  {/* Floating Send Icon */}
                   <FaPaperPlane
                     onClick={handleSendComment}
                     className="ml-3 text-gray-300 hover:text-gray-200 hover:duration-300 cursor-pointer"
