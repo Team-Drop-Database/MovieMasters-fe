de1231af920e2d4cfc780b32829cc944b9f928e5
Thomas Linssen <t.linssen@live.nl>
2025-01-19 11:25:59 +0100

    Added the 'explore' page

---
+++ src/app/(pages)/explore/page.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import Genre from "@/models/Genre";
+import Movie from "@/models/Movie";
+import { getMovieGenres, getMoviesByGenre } from "@/services/MovieService"
+import { useEffect, useState } from "react";
+
+export default function Explore() {
+
+    const [genres, setGenres] = useState<Genre[]>([]);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        async function fetchGenres() {
+            try {
+                const genres: Genre[] = await getMovieGenres();
+                setGenres(genres);
+            } catch (err: unknown) {
+            if (err instanceof Error) {
+                setError(err.message);
+                } else {
+                setError("An unknown error occurred.");
+                }
+            }
+        }
+
+        fetchGenres();
+    })
+
+    const genreSections: JSX.Element[] = [];
+    genres.forEach(genre => {
+
+        // const moviesByGenre: Movie[] = getMoviesByGenre([genre.name]);
+        const content = 
+            <div>
+                <h3>{genre.name}</h3>
+            </div>;
+        genreSections.push(content);
+    });
+
+    if(error) {
+        return <div>An error occurred.</div>;
+    }
+
+    return <div>
+                {genreSections}
+           </div>;
+}
