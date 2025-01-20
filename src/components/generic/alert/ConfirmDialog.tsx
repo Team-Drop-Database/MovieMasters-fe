21dd2523bb17528810e5618d61402d8085402ec7
Mitchell Tol <mitchell.tol@hva.nl>
2025-01-20 12:39:54 +0100

    Feature: report on review

--- src/components/generic/alert/ConfirmDialog.tsx
+++ src/components/generic/alert/ConfirmDialog.tsx
@@ -1,22 +1,40 @@
 import React from "react";
+import BigTextField from "../BigTextField";
 
-interface ConfirmDialogProps {
-  message: string;
-  onConfirm: () => void;
-  onCancel: () => void;
+type TextFieldProps = {
+  value: string
+  onChange: (newValue: string) => void
+  placeholder?: string
 }
 
-const ConfirmDialog: React.FC<ConfirmDialogProps> = ({ message, onConfirm, onCancel }) => {
+interface ConfirmDialogProps {
+  message: string
+  onConfirm: () => void
+  onCancel: () => void
+  textFieldProps?: TextFieldProps
+}
+
+const ConfirmDialog: React.FC<ConfirmDialogProps> = (
+  { message, onConfirm, onCancel, textFieldProps }
+) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
       <div className="p-6 bg-white rounded shadow-lg w-full max-w-sm">
         <p className="mb-4 text-center text-gray-800">{message}</p>
+        { textFieldProps && (
+          <BigTextField
+            value={textFieldProps.value}
+            onValueChange={textFieldProps.onChange}
+            placeholder={textFieldProps.placeholder}
+            className="bg-light_grey_active w-full"
+          />
+        )}
         <div className="flex justify-center space-x-4">
           <button
             onClick={onConfirm}
             className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
           >
-            Delete
+            Confirm
           </button>
           <button
             onClick={onCancel}
