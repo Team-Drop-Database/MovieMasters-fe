057c882ce95d92683aa142551b8adb8ed50f6d2c
Thomas Linssen <thomas.linssen@hva.nl>
2025-01-20 20:33:55 +0100

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Thomas Tol <thomas.tol@hva.nl>

--- src/app/(pages)/explore/page.tsx
+++ src/app/(pages)/explore/page.tsx
@@ -81,7 +81,7 @@ export default function Explore() {
                 }
             }
         }
-        fetchMovieLists();
+        void fetchMovieLists();
         setLoading(false);
     }, [genres])
 
